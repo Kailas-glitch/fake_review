cadc474b2ab9a07f567baacbdfb13debcefcaff4
kailas <kailudasu3@gmail.com>
2026-03-22 22:09:43 +0530

    Fix Python version and dependencies

--- test_model.py
+++ test_model.py
@@ -1,4 +1,6 @@
 from tensorflow.keras.models import load_model
 
 model = load_model("models/fake_review_model.h5")
-print("Working!")
+model.save("fake_review_model.keras")
+
+print("Conversion done!")
